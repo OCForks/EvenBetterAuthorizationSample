8f7fdc0dcb689526ada8a6fe63f62fbecc79b146
Prez Cannady <revprez@opencorrelate.org>
2014-12-31 02:52:03 -0500

    Install scripts

---
+++ Install.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+export project_name="EvenBetterAuthorizationSample"
+export app_name="${project_name}"
+export app_info="App/App-Info.plist"
+export app_signed="./build/Release/${app_name}.app"
+
+export helper_name="HelperTool"
+export identifier="com.example.apple-samplecode.EBAS"
+export helper_info="${helper_name}/${helper_name}-Info.plist"
+
+export smjobblessutil="./SMJobBlessUtil.py"
+
+
+build() 
+{
+
+    xcodebuild clean -project $project_name.xcodeproj > /dev/null && xcodebuild -project $project_name.xcodeproj > /dev/null
+
+}
+
+sign()
+{
+
+    /usr/bin/env python $smjobblessutil setreq $app_signed $app_info $helper_info  
+
+}
+
+check()
+{
+    /usr/bin/env python $smjobblessutil check $app_signed 
+}
+
+
+build && sign && build && check 
